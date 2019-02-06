a0a3dc4938ac3943e37d5e39ad468c529e98a494
haoxingxing <mr.haoxx@outlook.com>
2019-02-06 15:57:52 +0800

    checkpoint

--- ProxyServer/main.cpp
+++ ProxyServer/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char* argv[])
 			++thread_cout;
 			s2c.detach();
 			++thread_cout;
-			std::cout << thread_cout;
+			//std::cout << thread_cout;
 			//printVector();
 		}
 	}
