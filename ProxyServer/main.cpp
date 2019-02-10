e0944b8412d3c1ab542a1ed203a011a7d1ee0b8f
haoxingxing <mr.haoxx@outlook.com>
2019-02-10 12:01:17 +0800

    fix *1

--- ProxyServer/main.cpp
+++ ProxyServer/main.cpp
@@ -219,9 +219,6 @@ int main(int argc, char* argv[])
 	if (ThisPort == 0) { std::cout << "Local Port Not Found"; usage(); return -1; }
 	if (std::string(g_key) != "password")
 		isusingaes = true;
-	for (int i = 0; i < 128; ++i)
-		init_table(std::string(1, (char)i), true);
-
 	signal(SIGINT, stop);
 	signal(SIGPIPE, SIG_IGN);
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
